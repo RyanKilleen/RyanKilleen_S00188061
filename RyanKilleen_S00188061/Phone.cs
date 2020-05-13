57ef5b4223c2bb10c28b4b2a23238d35feca9c88
RyanKilleen <46888783+RyanKilleen@users.noreply.github.com>
2020-05-13 10:13:01 +0100

    GitHub Setup and Phone Class added With Properties

---
+++ RyanKilleen_S00188061/Phone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RyanKilleen_S00188061
+{
+    class Phone
+    {
+        public String Name { get; set; }
+        public int Price { get; set; }
+        public String OperatingSystem { get; set; }
+        public string OS_Image { get; set; }
+        public string Phone_Image { get; set; }
+
+    }
+}
