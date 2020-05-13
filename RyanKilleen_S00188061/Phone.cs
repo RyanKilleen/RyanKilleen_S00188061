c9010d9315caf00ee4dc073ea40d1474375e159d
RyanKilleen <46888783+RyanKilleen@users.noreply.github.com>
2020-05-13 10:50:16 +0100

    Increase price method added

--- RyanKilleen_S00188061/Phone.cs
+++ RyanKilleen_S00188061/Phone.cs
@@ -9,10 +9,32 @@ namespace RyanKilleen_S00188061
     class Phone
     {
         public String Name { get; set; }
-        public int Price { get; set; }
+        public double Price { get; set; }
         public String OperatingSystem { get; set; }
         public string OS_Image { get; set; }
         public string Phone_Image { get; set; }
 
+    }
+
+    public void IncreasePrice(double Price, long previousValue, long currentValue)
+    {
+        var trend = "10%";
+
+        if(previousValue < currentValue)
+        {
+            if(previousValue != 0)
+            {
+                var difference = previousValue - currentValue;
+                var pctIncrease = (double)(difference / previousValue) * 100;
+                trend = pctIncrease.ToString("P");
+            }
+            else
+            {
+                trend = currentValue.ToString("P");
+            }
+
+            return trend;
+
+        }
     }
 }
